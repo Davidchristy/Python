1408b89ea1dd5fc6c618419ffe5a34ef85763290
David Christy <Davidchristy@email.arizona.edu>
2014-04-09 14:15:49 -0700

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,8 +1,15 @@
+'''
+This is a pay of using geometric rules for triangles and circles to find pi.
+It's super slow and isn't perfect, but one saturday morning I missed my bus
+and needed something to fill my time.
+'''
+
 import random
 import math
 total = 0
 inside = 0
-for x in range(1000000):
+aprx = 1000000
+for x in range(aprx):
 	if math.sqrt(random.random()**2+random.random()**2) < 1: inside+=1
 	pi = (inside/(x+1))*4
 print(pi)
