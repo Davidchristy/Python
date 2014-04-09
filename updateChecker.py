b70eb27f713a916e163915e473fbd9f4410b0ffb
David Christy <Davidchristy@email.arizona.edu>
2014-04-09 14:19:06 -0700

    Update updateChecker.py

--- updateChecker.py
+++ updateChecker.py
@@ -1,10 +1,18 @@
+'''
+A quick script I wrote to tell if a website code has been changed,
+of was meant for websites that change often, because its very wasteful, 
+but gives you instant results.
+'''
+
 import urllib.request
 
-f = urllib.request.urlopen("http://www.google.com")
+website = "http://www.google.com"
+
+f = urllib.request.urlopen(website)
 current = f.read()
 
 while True:
-	f = urllib.request.urlopen("http://www.google.com")
+	f = urllib.request.urlopen(website)
 	temp = f.read()
 	if(current != temp):
 		break
