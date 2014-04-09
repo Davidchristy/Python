5eba18c1a7bb289cd5166fe1183a308b2ae6274c
David Christy <Davidchristy@email.arizona.edu>
2014-04-09 14:11:17 -0700

    Update IP_Finder.py

--- IP_Finder.py
+++ IP_Finder.py
@@ -1,10 +1,15 @@
+'''
+A small program to put the Local and Global IP on the same screen.
+I needed to know both for a project and wrote this up to save time.
+'''
+
 import urllib.request
 import socket
 import urllib
 
 print("Local IP : " + socket.gethostbyname(socket.gethostname()))
 inFile = str(urllib.request.urlopen("http://www.biranchi.com/ip.php").read())
-print("File without itereating over: "+ str(inFile))
+# print("File without itereating over: "+ str(inFile))
 ip = "Global IP: "
 for i in inFile:
 	if i.isdigit() or i == ".":
